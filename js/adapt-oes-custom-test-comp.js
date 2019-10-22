1c40ea54efd29427a2e41bff8104ed84cbf97d6e
rDakarapu-oes <rDakarapu@oes.edu.au>
2019-10-22 12:07:50 +1100

    testing adapt custom component

---
+++ js/adapt-oes-custom-test-comp.js
@@ -0,0 +1,34 @@
+define([
+    "core/js/adapt",
+    "core/js/views/componentView",
+    "core/js/models/ComponentModel"
+], function(Adapt, ComponentView) {
+
+    var ComponentName = ComponentView.extend({
+
+        preRender: function() {
+            console.log("preRender");
+            //here is a good place to add any code you want to run prior to the component being rendered
+        },
+
+        postRender: function() {
+            console.log("postRender");
+            // IMPORTANT!
+            // Both of the following methods need to be called inside your view.
+
+            // Use this to set the model status to ready.
+            // It should be used when telling Adapt that this view is completely loaded.
+            // This is sometimes used in conjunction with imageReady.
+            this.setReadyStatus();
+
+            // Use this to set the model status to complete.
+            // This can be used with inview or when the model is set to complete/the question has been answered.
+            this.setCompletionStatus();
+        }
+
+    }, {
+        template: "oes-custom-test-comp"
+    });
+
+    return Adapt.register("oes-custom-test-comp", ComponentName);
+});
